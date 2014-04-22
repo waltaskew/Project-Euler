41775599d3a18f954bf67ba167d86657d8be9a1f
waltaskew <waltaskew@gmail.com>
2014-04-22 12:19:23 -0500

    clean up

--- Twelve.hs
+++ Twelve.hs
@@ -1,7 +1,17 @@
-import qualified Prime
-import qualified List
+import Prime (factorCount)
+
 
 triangleNumbers :: [Integer]
-triangleNumbers = 1 : zipWith (+) [2 ..] triangleNumbers
+{-Triangle numbers are the sums of natural numbers.
+1
+1 + 2
+1 + 2 + 3
+1 + 2 + 3 + 4
+and so on.
+We define the sequence of triangle numbers here as the sum of
+the previous triangle number and the next natural number.-}
+triangleNumbers = 1 : zipWith (+) [2..] triangleNumbers
 
-answer = head (dropWhile ((500 > ) . Prime.factorCount) triangleNumbers)
+answer = head $ dropWhile ((500 > ) . factorCount) triangleNumbers
+
+main = do print answer
