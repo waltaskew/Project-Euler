8cee74fa7bd9028a32e44d7a3a6a2aadd86f72c5
waltaskew <waltaskew@gmail.com>
2014-04-22 16:06:10 -0500

    memoize this thing

--- Fourteen.hs
+++ Fourteen.hs
@@ -1,20 +1,38 @@
-import qualified Data.List as List
+import Data.List (foldl', maximumBy)
+import Data.Function (on)
+import qualified Data.Map as Map
 
-main = do
-     print answer
-
-iterSequence n = buildSequence n []
+collatz :: Int -> Map.Map Int [Int] -> ([Int], Map.Map Int [Int])
+{-Produce the Collatz sequence starting with the given number, using
+the given Map to memoize Collatz sequences.  Returns a tuple of the
+sequence and an updated map for newly encountered Collatz sequences.-}
+collatz 1 collatzMap = ([1], collatzMap)
+collatz n collatzMap = (n: completeSeries, completeMap)
     where
-      buildSequence 1 acc = reverse (1 : acc)
-      buildSequence n acc = buildSequence (next n) (n : acc)
-      next n
-          | even n = div n 2
-          | otherwise = 3 * n + 1
+      (completeSeries, completeMap) = case Map.lookup n collatzMap of
+        Just v -> (v, collatzMap)
+        Nothing -> (newSeries, Map.insert n newSeries newMap)
+      (newSeries, newMap) = collatz (nextCollatz n) collatzMap
 
-answer = List.foldl' holdMax (0, 0)  [1 .. 999999]
-    where
-      holdMax acc@(max, num) n =
-          if sequenceLength > max
-          then (sequenceLength, n)
-          else acc
-              where sequenceLength = length . iterSequence $ n
+nextCollatz :: Int -> Int
+{-Return the next number in the Collatz sequence-}
+nextCollatz n
+  | even n = div n 2
+  | otherwise = 3 * n + 1
+
+getCollatzs :: [Int] -> [[Int]]
+{-Return the Collatz sequence for each number in the given list of numbers.-}
+getCollatzs xs = doGetCollatzs xs Map.empty
+  where
+    doGetCollatzs [] _ = []
+    doGetCollatzs (x:xs) collatzMap = series : doGetCollatzs xs newMap
+      where
+        (series, newMap) = collatz x collatzMap
+
+answer = fst $ maximumBy (compare `on` collatzLength)  $ zip numbers collatzSequences
+  where
+    collatzLength = length . snd
+    numbers = [1..999999]
+    collatzSequences = getCollatzs numbers
+
+main = do print answer
